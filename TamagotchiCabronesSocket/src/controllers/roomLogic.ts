b059170ea6ed4efdda1053376b93676e53e8eeea
IsCa3215 <ismayalica@gmail.com>
2025-01-09 22:35:40 +0100

    y tal

--- TamagotchiCabronesSocket/src/controllers/roomLogic.ts
+++ TamagotchiCabronesSocket/src/controllers/roomLogic.ts
@@ -1,16 +1,21 @@
+import { Jugador } from "../models/Jugadores";
 import { Room } from "../models/Rooms";
 
-interface Salas {
-    salas: Room[];
-}
-
-function createRoom(salas: Salas) {
-    return {
-        id: salas.salas.length + 1,
-        players: []
+class roomLogic {
+    private rooms: Room[] = [];
+    private id: Room = { id: 0, players: [] };
+    constructor(room: number) {
+        this.rooms = [];
     }
-}
+    createRoom() {
+        const room = new roomLogic(this.rooms.length + 1);
+        this.rooms.push(room);
+        return room;
+    }
 
-export function joinRoom(room: Room){
-    
+    joinRoom(room: Room) {
+        if (room.players.length < 2) {
+            room.players.push();
+        }
+    }
 }
